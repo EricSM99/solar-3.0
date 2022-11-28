e1987f976a5acf6fa4a329d2dfbe5c0d9861d234
EricSM99 <ericmaciel999@gmail.com>
2022-11-28 18:10:17 -0300

    atualizacao

--- Simulador/simulador.js
+++ Simulador/simulador.js
@@ -1,33 +1,109 @@
+function chuveiro(){
+    var hchuveiro = window.document.getElementById('hchuveiro').value
+    var qchuveiro = window.document.getElementById('qchuveiro').value
+
+    var chuveiro = hchuveiro * 3
+    var chuveiro = chuveiro * qchuveiro
+
+    alert("Adicionado com sucesso!");
+
+    
+
+}
+
+function tv(){
+    var htv = window.document.getElementById('htv').value
+    var qtv = window.document.getElementById('qtv').value
+
+    var tv = htv * 98
+    var tv1 = tv * qtv
+
+    alert("Adicionado com sucesso!");
+
+}
+
+function monitor(){
+
+    var hmonitor = window.document.getElementById('hmonitor').value
+    var qmonitor = window.document.getElementById('qmonitor').value
+
+    
+
+    var monitor = hmonitor * 10
+    var monitor1 = monitor * qmonitor
+
+    alert("Adicionado com sucesso!");
+
+    
+
+}
+
 function geladeira(){
 
     var hgeladeira = window.document.getElementById('hgeladeira').value
-var qgeladeira = window.document.getElementById('qgeladeira').value
+    var qgeladeira = window.document.getElementById('qgeladeira').value
 
-var res = window.document.getElementById('res')
+    
 
-var geladeira = hgeladeira * 53
-var geladeira1 = geladeira * qgeladeira
+    var geladeira = hgeladeira * 53
+    var geladeira1 = geladeira * qgeladeira
 
-res.innerHTML = `<p>Consumo diario de ${geladeira1}(kWh)`
+    alert("Adicionado com sucesso!");
 
 }
 
 function arcondicionado(){
 
     var harcondicionado = window.document.getElementById('hgeladeira').value
-var qarcondicionado = window.document.getElementById('qgeladeira').value
+    var qarcondicionado = window.document.getElementById('qgeladeira').value
 
-var res = window.document.getElementById('res')
+    
 
-var arcondicionado = harcondicionado * 33
-var arcondicionado1 = arcondicionado * qarcondicionado
+    var arcondicionado = harcondicionado * 33
+    var arcondicionado1 = arcondicionado * qarcondicionado
 
-res.innerHTML = `<p>Consumo diario de ${arcondicionado1}(kWh)`
-
-}
-
-function finalizar(){
-
+    alert("Adicionado com sucesso!");
 
 }
+
+function resultado() {
+
+    var hchuveiro = window.document.getElementById('hchuveiro').value
+    var qchuveiro = window.document.getElementById('qchuveiro').value
+
+    var chuveiro = hchuveiro * 98
+    var chuveiro1 = chuveiro * qchuveiro
+
+    var htv = window.document.getElementById('htv').value
+    var qtv = window.document.getElementById('qtv').value
+
+    var tv = htv * 98
+    var tv1 = tv * qtv
+
+    var hmonitor = window.document.getElementById('hmonitor').value
+    var qmonitor = window.document.getElementById('qmonitor').value
+
+    var monitor = hmonitor * 10
+    var monitor1 = monitor * qmonitor
+
+
+    var hgeladeira = window.document.getElementById('hgeladeira').value
+    var qgeladeira = window.document.getElementById('qgeladeira').value
+
+   
+    var geladeira = hgeladeira * 53
+    var geladeira1 = geladeira * qgeladeira
+
+    
+    var harcondicionado = window.document.getElementById('hgeladeira').value
+    var qarcondicionado = window.document.getElementById('qgeladeira').value
+
+
+    var arcondicionado = harcondicionado * 33
+    var arcondicionado1 = arcondicionado * qarcondicionado
+
+    var result = geladeira1 + arcondicionado1 + monitor1 + tv1 + chuveiro1;
+    var res = window.document.getElementById('res')
+    res.innerHTML = `<p>Consumo diario de ${result}(kWh)`
+  }
 
